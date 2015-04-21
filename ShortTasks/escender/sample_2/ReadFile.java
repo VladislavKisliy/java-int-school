5401586c416d8918b549e0b90a4ab17f396d8faf
Escender <https://github.com/dreamesc/test>
2015-04-21 10:18:59 +0300

    2 task

---
+++ ShortTasks/escender/sample_2/ReadFile.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sample_2;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Dreamesc
+ */
+public class ReadFile {
+    
+    private String fileName;
+    
+    public ReadFile(String fileName) {
+        this.fileName = fileName;
+    }
+    
+    public List<String> read(List<String> arrList) {        
+        try {
+            File f = new File(fileName);            
+            FileReader fr = new FileReader(f);
+            
+            BufferedReader br = new BufferedReader(fr);
+            String line = br.readLine();
+            while (line != null) {
+                arrList.add(line);
+                line = br.readLine();
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(ReadFile.class.getName()).log(Level.SEVERE, null, ex);
+            
+        } catch (IOException ex) {
+            Logger.getLogger(ReadFile.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            return arrList;
+        }
+            
+    }
+        
+}
---
+++ ShortTasks/escender/sample_2/WriteFile.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sample_2;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Dreamesc
+ */
+public class WriteFile {
+
+    private String fileName;
+    
+    public WriteFile(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public void write(List<String> arrList) {
+        BufferedWriter bw = null;
+        try {
+            OutputStreamWriter fw = new OutputStreamWriter(new FileOutputStream(new File(fileName)), "UTF8");
+            //FileWriter fw = new OutputStreamWrteFileWriter(new File(xFile), "UTF-8");            
+            bw = new BufferedWriter(fw);
+
+            for (String line : arrList) {
+                bw.write(line);
+                bw.newLine();
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(WriteFile.class.getName()).log(Level.SEVERE, null, ex);
+
+        } catch (IOException ex) {
+            Logger.getLogger(WriteFile.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                if (bw != null) {
+                    bw.close();
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(WriteFile.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+}
