c5e07f36efffe746fe736e7cbda23e049bc5ddfd
dreamesc <dreamesc@gmail.com>
2015-06-08 23:11:45 +0300

    TTT v1

---
+++ escender/TicTacToe/src/main/java/com/weigandtconsulting/javaschool/escender/Showable.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.weigandtconsulting.javaschool.escender;
+
+import java.util.List;
+
+/**
+ *
+ * @author W
+ */
+public interface Showable {
+    void refreshBattleField(List<GameState> battleField);
+}
