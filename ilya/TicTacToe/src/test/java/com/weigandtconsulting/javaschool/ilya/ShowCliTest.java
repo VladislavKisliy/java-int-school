318e9e7db77051d69d3a04c98fae68d01de1ac81
Ilya Beketov <ilya.beketov@gmail.com>
2015-06-16 09:30:50 +0300

    New

---
+++ ilya/TicTacToe/src/test/java/com/weigandtconsulting/javaschool/ilya/ShowCliTest.java
@@ -0,0 +1,13 @@
+package com.weigandtconsulting.javaschool.ilya;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by ibeketov on 10/06/15.
+ */
+public class ShowCliTest extends TestCase {
+
+    public void testRefreshBattleField() throws Exception {
+
+    }
+}
