1760bf417de801c3d9c1278ed5754c3c1464ac69
Oleg <Oleg@PC-ANTARES>
2015-08-04 03:27:01 +0400

    Porting WinOrDie project to Maven. Done

--- oleg/WinOrDie/src/main/java/com/wdt/java/AIClass.java
+++ oleg/WinOrDie/src/main/java/com/wdt/java/AIClass.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package winordie;
+package com.wdt.java;
 
 import static java.lang.Math.pow;
 import java.util.ArrayList;
