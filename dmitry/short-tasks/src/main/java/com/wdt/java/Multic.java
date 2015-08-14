42fc13a3ae1b71776334ce12842fabaf45e51583
Dmitry Kuzmin <dmitry.kuzmin@weigandt-consulting.com>
2015-08-14 16:58:44 +0300

    Multithread increment

---
+++ dmitry/short-tasks/src/main/java/com/wdt/java/Multic.java
@@ -0,0 +1,32 @@
+package com.weigandtconsulting.javaschool.dmitry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Multic extends Thread{
+	private static int var = 0;
+	
+	public void run(){
+		System.out.println("Thread has been started");
+		this.incVar();
+	}
+
+	public void incVar(){
+		var++;
+	}		
+	
+	public void printVar(){
+		System.out.println("var = " + var);
+	}
+	
+	public static void main(String[] args) throws InterruptedException{
+		System.out.println("App has been started");
+		List<Multic> threadList  = new ArrayList<Multic>();
+		for (int i=0; i<10; i++){
+			threadList.add(new Multic());
+			threadList.get(i).start();
+		}
+		threadList.get(0).printVar();
+		System.out.println("App is finished");
+	}
+}
