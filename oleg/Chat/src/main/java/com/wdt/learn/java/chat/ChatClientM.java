811176fa743f10ec0e806f07cd9fc07f56418ec8
thirstfortruth <thirstfortruth@yandex.ru>
2015-08-27 23:44:16 +0300

    Chat modification. No success for now.

--- oleg/Chat/src/main/java/com/wdt/learn/java/chat/ChatClientM.java
+++ oleg/Chat/src/main/java/com/wdt/learn/java/chat/ChatClientM.java
@@ -6,10 +6,35 @@
 
 package com.wdt.learn.java.chat;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 /**
  *
  * @author Oleg 
  */
 public class ChatClientM {
-
+    String name;
+    public ChatClientM(String inName, String serverName, int serverPort){
+        this.name=inName;
+        try{
+        Socket socket = new Socket(serverName,serverPort);
+        PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            while(in.readLine()!=null){
+                out.print(System.in);
+            }
+        } catch (UnknownHostException e) {
+        System.out.println("Unknown host:"+ChatConfiguration.getServerName());
+        System.exit(1);
+        } catch  (IOException e) {
+        System.out.println("No I/O");
+        System.exit(1);
+        }
+    }
+    
 }
--- oleg/Chat/src/main/java/com/wdt/learn/java/chat/ChatServerM.java
+++ oleg/Chat/src/main/java/com/wdt/learn/java/chat/ChatServerM.java
@@ -6,21 +6,42 @@
 
 package com.wdt.learn.java.chat;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.Writer;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.channels.Channels;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Oleg 
  */
 public class ChatServerM {
-    
-    public void InitializeServer(int serverPort) throws IOException{
-        ServerSocket server = new ServerSocket(serverPort);
-    }
+
     public Socket getClientSocket(ServerSocket server) throws IOException{
         return server.accept();
     }
-    
+    public String listenClentTalk(Socket client) throws IOException{
+        BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        return in.readLine(); 
+    }
+    public PrintWriter writeToClient(Socket client) throws IOException{
+        PrintWriter out = new PrintWriter(client.getOutputStream(),true);
+        return out;
+    }
+    public static void main(String[] argc) {
+        ChatConfiguration.setServerName("localhost");
+        ChatConfiguration.setServerPort(2047);
+        try{
+            ServerSocket server = new ServerSocket(ChatConfiguration.getServerPort());
+            Socket client = getClientSocket(server);
+        }catch(IOException e){
+            
+        }
+    }
 }
