33e9e206d512c038af24ca4a7848cd03ebcfa237
W <W@192.168.5.50>
2015-09-03 09:38:28 +0300

    ver 2: added quit functionality

--- escender/short-tasks/src/main/java/com/wdt/mult/FixedTreadPool.java
+++ escender/short-tasks/src/main/java/com/wdt/mult/FixedTreadPool.java
@@ -4,45 +4,59 @@
  */
 package com.wdt.mult;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.concurrent.*;
 
 /**
  *
  * @author Dreamesc
  */
 public class FixedTreadPool extends Thread {
-    
+
+    public static Queue<Integer> concurrentQueue = new ConcurrentLinkedQueue<Integer>();
+
+    public static void main(String args[]) throws InterruptedException {
+        System.out.println("Start Main");
+        List<Future> arrFutures = new LinkedList<Future>();
+        ExecutorService pool = Executors.newFixedThreadPool(5);
+        Random rn = new Random();
+        for (int i = 0; i < 10; i++) {
+            arrFutures.add(pool.submit(new FixedTreadPool(rn.nextInt(3000))));
+        }
+
+        int i = 0;
+        while (!arrFutures.isEmpty()) {
+            i = (i + 1) % arrFutures.size();
+            if (arrFutures.get(i).isDone()) {
+                arrFutures.remove(i);
+                System.out.println("End thread with sleep time:" + concurrentQueue.poll());
+            };
+        }
+
+        pool.shutdown();
+        System.out.println("End Main");
+    }
     private Integer waitTime;
 
     public FixedTreadPool(Integer waitTime) {
         this.waitTime = waitTime;
-    }   
-    
+    }
+
     public void run() {
-        System.out.println("Start thread with sleep time in second - " + waitTime);
+        System.out.println("Start thread with sleep time in millisecond - " + waitTime);
         try {
-            TimeUnit.SECONDS.sleep(waitTime);
+            TimeUnit.MILLISECONDS.sleep(waitTime);
         } catch (InterruptedException ex) {
             Logger.getLogger(FixedTreadPool.class.getName()).log(Level.SEVERE, null, ex);
         }
-        System.out.println("End thread - " + waitTime);
-    }
-            
-    public static void main (String args[]) throws InterruptedException {
-        System.out.println("Start Main");
-        ExecutorService pool = Executors.newFixedThreadPool(3);
-        Random rn = new Random();
-        for (int i = 0; i < 10; i++) {
-            pool.submit(new FixedTreadPool(rn.nextInt(10)));
-            //System.out.println();
-        }
-        pool.awaitTermination(10, TimeUnit.SECONDS);
-        pool.shutdownNow();
-        System.out.println("End Main");
+        concurrentQueue.add(waitTime);
     }
 }
