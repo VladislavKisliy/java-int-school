c6504c7500dd8c77c8c4258507f990fbba7a7a47
Escender <escender.sary@weigandt-consulting.com>
2015-09-14 01:28:54 +0300

    Chat v2

--- escender/short-tasks/src/main/java/com/wdt/chat/ChatServer.java
+++ escender/short-tasks/src/main/java/com/wdt/chat/ChatServer.java
@@ -4,11 +4,16 @@
  */
 package com.wdt.chat;
 
+import static com.wdt.chat.ChatServer.pool;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.logging.Level;
@@ -19,46 +24,75 @@ import java.util.logging.Logger;
  * @author Dreamesc
  */
 public class ChatServer extends Thread {
-
+    
+    static List<ChatServer> clientList;
     private Socket socket;
-
-    public ChatServer(Socket socket) {
+    private DataInputStream dataInStream;
+    private DataOutputStream dataoutStream;
+    private Integer index;
+    
+    public ChatServer(Socket socket, Integer index) {
         this.socket = socket;
+        this.index = index;
+    }
+    static ExecutorService pool;
+    
+    static void remove(Integer index) {
+        clientList.remove(index);
     }
 
-    static ExecutorService pool;
     public void run() {
-        DataInputStream dataInStream;
+        System.out.println("Server Point 2");
         try {
             dataInStream = new DataInputStream(socket.getInputStream());
-
-            DataOutputStream dataoutStream;
-
             dataoutStream = new DataOutputStream(socket.getOutputStream());
-
-
+            
             String line;
             while (true) {
                 line = dataInStream.readUTF();
                 System.out.println("Client sent: " + line);
-                dataoutStream.writeUTF(line); // отсылаем клиенту обратно ту самую строку текста.
-                
-                dataoutStream.flush(); // заставляем поток закончить передачу данных.
+                System.out.println("Client 3:" + clientList.size() + " ind:" +index);
+                for (ChatServer client : clientList) {
+                    client.broadcast(line);
+                }
+                if (line.equals("buy")) {
+                    System.out.println("Client 4:" + clientList.size() + " ind:" +index);
+                    dataInStream.close();
+                    dataoutStream.close();
+                    remove(index);//clientList.remove(index);
+                    System.out.println("Client 5:" + clientList.size() + " ind:" +index);
+                    return;
+                }
             }
         } catch (IOException ex) {
             Logger.getLogger(ChatServer.class.getName()).log(Level.SEVERE, null, ex);
         }
+        
     }
-
+    
+    private void broadcast(String line) throws IOException {
+        dataoutStream.writeUTF(line);        
+        dataoutStream.flush();        
+    }
+    
     public static void main(String[] args) throws IOException {
-
         pool = Executors.newFixedThreadPool(10);
-
+        
+        
+        clientList = new LinkedList<ChatServer>();//Collections.synchronizedList();//new ArrayList<ChatServer>();
         ServerSocket serverSocket = new ServerSocket(8081);
+        try {
+            
         while (true) {
+            
             System.out.println("Server Point 1");
-            pool.submit(new ChatServer(serverSocket.accept()));            
-            System.out.println("Server Point 2");
+            ChatServer client = new ChatServer(serverSocket.accept(), clientList.size());
+            clientList.add(client);
+            pool.submit(client);
+            
+        }
+        } finally {
+            serverSocket.close();
         }
     }
 }
