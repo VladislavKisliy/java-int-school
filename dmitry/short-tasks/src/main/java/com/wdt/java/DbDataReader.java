2c30a72f8b3c8ff6c060962cff8dedd8c3ebdbda
Dmitry Kuzmin <dmitry.kuzmin@weigandt-consulting.com>
2015-10-02 10:08:19 +0300

    jdbc, rowset usage

--- dmitry/short-tasks/src/main/java/com/wdt/java/DbDataReader.java
+++ dmitry/short-tasks/src/main/java/com/wdt/java/DbDataReader.java
@@ -1,12 +1,17 @@
 package com.wdt.java;
 
 import java.sql.Connection;
-import java.sql.Driver;
+//import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
+
+import javax.sql.rowset.JdbcRowSet;
+
+import com.sun.rowset.JdbcRowSetImpl;
 
 public class DbDataReader {
 	public static String padRight(String s, int n) {
@@ -27,15 +32,21 @@ public class DbDataReader {
         System.out.println("connected to database...");
         
         String sql ="select customer, customer_name, cre_date from kws.customer_master where rownum <= 10";
-        PreparedStatement preStatement = conn.prepareStatement(sql);        
+        PreparedStatement preStatement = conn.prepareStatement(sql, ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);        
         ResultSet result = preStatement.executeQuery();
 
         while(result.next()){
             System.out.println(result.getLong(1)+" | "+padRight(result.getString("customer_name"),35)+" | "+result.getDate(3));
         }
+        
+        JdbcRowSet jdbcRs = new JdbcRowSetImpl(result);
+        jdbcRs.last();
+        System.out.println("Row_number for the last row:"+jdbcRs.getRow());
+        
         System.out.println("done");
-
-       conn.close();
+        
+        jdbcRs.close();
+        conn.close();
 	}	
 
 }
